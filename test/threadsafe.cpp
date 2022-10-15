e11a74458f5310e6acd62d467ace5a14f26aed03
Bernd Porr <mail@berndporr.me.uk>
2022-10-15 15:00:06 +0100

    Made the treadsafety test a bit more stressful!

--- test/threadsafe.cpp
+++ test/threadsafe.cpp
@@ -5,19 +5,17 @@
 #include <limits>
 #include <vector>
 #include <thread>
-#include <chrono>
 
 #include <cxx-spline.hpp>
 
 
-std::vector<double> const knots = { 1, 2 };
-std::vector<double> const values = { 10, 20 };
+std::vector<double> const knots = { 0, 1, 2, 3, 4, 5 };
+std::vector<double> const values = { 1, 2, 1, 0, 1, 2 };
 cubic_spline spline;
 
 void manycalc() {
-	for(int i = 0; i < 1000; i++) {
+	for(int i = 0; i < 100000; i++) {
 		spline.calc(knots, values, cubic_spline::natural);
-		std::this_thread::sleep_for(std::chrono::milliseconds(1));
 	}
 }
 
@@ -26,13 +24,30 @@ TEST_CASE("Thread safety")
 {
 	spline.calc(knots, values, cubic_spline::natural);
 	std::thread t1(manycalc);
-	for(int i = 0; i < 1000; i++) {
-		CHECK(spline(0.5) == Approx(5));
-		CHECK(spline(1) == Approx(10));
-		CHECK(spline(1.5) == Approx(15));
-		CHECK(spline(2) == Approx(20));
-		CHECK(spline(2.5) == Approx(25));
-		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+	for(int j = 0; j < 100000; j++) {
+		for (std::size_t i = 0; i < knots.size(); i++) {
+			auto const t = knots[i];
+			auto const x = values[i];
+			CHECK(spline(t) == Approx(x));
+		}
+
+		// Example points (reference: scipy.interpolate.CubicSpline)
+		std::vector<double> const queries = {
+			-0.5, 0.5, 1.5, 2.5, 3.5, 4.5, 5.5
+		};
+		std::vector<double> const expects = {
+			0.31339713, 1.68660287, 1.69019139,
+			0.30263158, 0.34928230,
+			1.55023923, 2.44976077
+		};
+		
+		for (std::size_t i = 0; i < queries.size(); i++) {
+			auto const t = queries[i];
+			auto const x = expects[i];
+			CHECK(spline(t) == Approx(x));
+		}
+		CHECK(spline.getLowerBound() == Approx(0));
+		CHECK(spline.getUpperBound() == Approx(5));
 	}
 	t1.join();
 }
